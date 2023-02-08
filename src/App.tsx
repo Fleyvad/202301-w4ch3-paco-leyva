a4f3f3ae087b2c8b248fc8ea1b5fa3a2a89d35b5
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-02-08 20:56:44 +0100

    create component header and link with app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header.test";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
